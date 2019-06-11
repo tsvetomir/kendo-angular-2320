bd4b9c9d1de18adc7b0add1477bae494f4a3ba4a
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-06-11 10:39:24 +0300

    add Kendo UI window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DialogsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
